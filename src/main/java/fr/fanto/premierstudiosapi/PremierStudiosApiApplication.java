75ab756c3ceb2ef8b6847bd4c9b305b2af2cee53
BuildTools <alex.busi@hotmail.fr>
2025-01-25 15:06:49 +0100

    first commit

---
+++ src/main/java/fr/fanto/premierstudiosapi/PremierStudiosApiApplication.java
@@ -0,0 +1,13 @@
+package fr.fanto.premierstudiosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierStudiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierStudiosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fanto/premierstudiosapi/PremierStudiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fanto.premierstudiosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierStudiosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
